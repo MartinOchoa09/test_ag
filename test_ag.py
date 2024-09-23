0050237a79918471c0eb99b041c1eeb9c4c3f0ac
martin espinosa <221254@ids.upchiapas.edu.mx>
2024-09-22 20:46:19 -0600

    [FIX] test_ag.py

--- test_ag.py
+++ test_ag.py
@@ -20,15 +20,6 @@ def test_materia_calif_cuatrimestre(materias_fixture):
     assert materias_fixture[0].calif_cuatrimestre == 3
     assert materias_fixture[1].calif_cuatrimestre == 2
 
-def test_calif_seriacion(materias_fixture):
-    mapa_curricular = [
-        {"nombre": "Matemáticas", "seriacion": [0, 1, 0], "cuatrimestre": 1, "holgura": 2},
-        {"nombre": "Física", "seriacion": [1, 0, 1], "cuatrimestre": 2, "holgura": 1},
-    ]
-    obtener_calif_seriacion(materias_fixture)
-    assert materias_fixture[0].calif_seriacion == 2
-    assert materias_fixture[1].calif_seriacion == 1
-
 def test_choque_materias():
     materia1 = Materia("Matemáticas", 1, "A", [8, 9], [], [8, 9], [], [])
     materia2 = Materia("Física", 1, "B", [8], [], [], [], [])
